0bdbd1ba44a94115c68c33138be5b9826694d546
Daniel Meza <danielmeza.4c@gmail.com>
2022-05-18 11:34:37 -0700

    pruebas a produts

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ProductsService } from 'src/app/services/product.service';
+import { generateManyProducts } from '../models/product.mock';
+import { ProductComponent } from '../product/product.component';
 
 import { ProductsComponent } from './products.component';
 
-xdescribe('ProductsComponent', () => {
+fdescribe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: jasmine.SpyObj<ProductsService>;
 
   beforeEach(async () => {
+
+    const prodServiceSpy = jasmine.createSpyObj('ProductsService', ['getAll']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ ProductsComponent, ProductComponent ],
+      providers: [
+        { provide: ProductsService, useValue: prodServiceSpy}
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +27,15 @@ xdescribe('ProductsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    productService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
   });
 
   it('should create', () => {
+    const productMock = generateManyProducts(3);
+    productService.getAll.and.returnValue(of(productMock));
+
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+    expect(productService.getAll).toHaveBeenCalled();
   });
 });
